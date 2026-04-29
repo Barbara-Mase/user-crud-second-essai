b6c9dea899bb5f1ae85387c1d188e50a7731bc4e
Barbara-Mase <mase.barbara@gmail.com>
2026-04-29 17:36:59 +0200

    checkUpadte::UserController à faire

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,1 +1,117 @@
 <?php
+
+class UserController extends AbstractController
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function list(): void
+    {
+        $um = new UserManager();
+        $users = $um->findAll();
+
+        $this->render('admin/users/list.html.twig', [
+            'users' => $users
+        ]);
+    }
+
+    public function details(int $id): void
+    {
+        $um = new UserManager();
+        $user = $um->findOne($id);
+
+        $this->render('admin/users/details.html.twig', [
+            'user' => $user
+        ]);
+    }
+
+    public function update(int $id) : void
+    {
+        $um = new UserManager();
+        $user = $um->findOne($id);
+
+        if(!empty($_SESSION["errors"]))
+        {
+            $errors = $_SESSION["errors"];
+            unset($_SESSION["errors"]);
+            $this->render('admin/users/update.html.twig', [
+                'user' => $user,
+                "errors" => $errors
+            ]);
+        }
+        else
+        {
+            $this->render('admin/users/update.html.twig', [
+                'user' => $user
+            ]);
+        }
+    }
+
+
+
+    public function checkUpdate(int $id): void
+    {
+        echo "UserController::checkUpdate<br>";
+    }
+
+    public function create(): void
+    {
+        if (!empty($_SESSION["errors"])) {
+            $errors = $_SESSION["errors"];
+            unset($_SESSION["errors"]);
+            $this->render('admin/users/create.html.twig', [
+                "errors" => $errors
+            ]);
+        } else {
+            $this->render('admin/users/create.html.twig', [
+
+            ]);
+        }
+    }
+
+
+    public function checkCreate(): void
+    {
+        $_SESSION["errors"] = [];
+
+        if (!empty($_POST["email"]) && !empty($_POST["password"]) && !empty($_POST["first_name"]) && !empty($_POST["last_name"])) {
+            // tous les champs sont remplis
+            $um = new UserManager();
+            $user = $um->findByEmail($_POST["email"]);
+
+            if ($user === null) {
+                // il n'existe pas, on peut le créer
+                $user = new User($_POST["email"], $_POST["password"], $_POST["first_name"], $_POST["last_name"]);
+                $ret = $um->create($user);
+
+                if ($ret) {
+                    unset($_SESSION["errors"]);
+                    header("Location: index.php?route=list-users");
+                } else {
+                    $_SESSION["errors"][] = "La création a échoué lors de l'écriture dans la base de données.";
+                    header("Location: index.php?route=create-user");
+                }
+            } else {
+                $_SESSION["errors"][] = "Un utilisateur avec cet email existe déjà.";
+                header("Location: index.php?route=create-user");
+            }
+        } else {
+            $_SESSION["errors"][] = "Au moins un champ obligatoire est manquant.";
+            header("Location: index.php?route=create-user");
+        }
+    }
+
+
+    public function delete(int $id) : void
+    {
+        $um = new UserManager();
+        $um->delete($id);
+        header("Location: index.php?route=list-users");
+    }
+
+
+
+}
--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+
+
+// charge l'autoload de composer
+require "vendor/autoload.php";
+
+session_start();
+
+//charge le contenu du .env dans $_ENV
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
+$router = new Router();
+$router->handleRequest($_GET);
