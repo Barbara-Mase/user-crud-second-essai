589417461adf7c15c6a0e12d6d194f1fb7305fe5
Barbara-Mase <mase.barbara@gmail.com>
2026-07-14 16:50:33 +0200

    Projet fini

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -2,37 +2,33 @@
 
 class UserController extends AbstractController
 {
-
     public function __construct()
     {
         parent::__construct();
     }
-
     public function list(): void
     {
+        //affiche la liste des utilisateurs
         $um = new UserManager();
         $users = $um->findAll();
-
         $this->render('admin/users/list.html.twig', [
             'users' => $users
         ]);
     }
-
     public function details(int $id): void
     {
+        //Présente le profil d'un utilisateur
         $um = new UserManager();
         $user = $um->findOne($id);
-
         $this->render('admin/users/details.html.twig', [
             'user' => $user
         ]);
     }
-
     public function update(int $id) : void
     {
         $um = new UserManager();
+        //Récupère un utilisateur grâce à findOne()
         $user = $um->findOne($id);
-
         if(!empty($_SESSION["errors"]))
         {
             $errors = $_SESSION["errors"];
@@ -49,26 +45,19 @@ class UserController extends AbstractController
             ]);
         }
     }
-
-
-
     public function checkUpdate(int $id): void
     {
         $_SESSION["errors"] = [];
-
         $um = new UserManager();
-
+        //Vérifie si tous les champs sont remplis
         if (!empty($_POST["email"]) && !empty($_POST["password"]) && !empty($_POST["first_name"]) && !empty($_POST["last_name"])) {
-
-            // Vérifier si l'email appartient déjà à un autre utilisateur
             $existingUser = $um->findByEmail($_POST["email"]);
-
             if ($existingUser === null || $existingUser->getId() === $id) {
+                //Créé un nouvel utilisateur afin de mettre à jour la ligne de la base de données
                 $user = new User($_POST["email"], $_POST["password"], $_POST["first_name"], $_POST["last_name"]);
-                $user->setId($id); // ← manquait dans ta version !
-
+                $user->setId($id);
                 $ret = $um->update($user);
-
+                //En cas de succès, redirection vers la liste des utilisateurs
                 if ($ret) {
                     unset($_SESSION["errors"]);
                     header("Location: index.php?route=list-users");
@@ -80,13 +69,11 @@ class UserController extends AbstractController
                 $_SESSION["errors"][] = "Un utilisateur avec cet email existe déjà.";
                 header("Location: index.php?route=update-user&id=$id");
             }
-
         } else {
             $_SESSION["errors"][] = "Au moins un champ obligatoire est manquant.";
             header("Location: index.php?route=update-user&id=$id");
         }
     }
-
     public function create(): void
     {
         if (!empty($_SESSION["errors"])) {
@@ -97,26 +84,21 @@ class UserController extends AbstractController
             ]);
         } else {
             $this->render('admin/users/create.html.twig', [
-
             ]);
         }
     }
-
-
     public function checkCreate(): void
     {
         $_SESSION["errors"] = [];
-
         if (!empty($_POST["email"]) && !empty($_POST["password"]) && !empty($_POST["first_name"]) && !empty($_POST["last_name"])) {
-            // tous les champs sont remplis
             $um = new UserManager();
+            //Vérifie se l'email est déjà présent en base
             $user = $um->findByEmail($_POST["email"]);
-
             if ($user === null) {
                 // il n'existe pas, on peut le créer
                 $user = new User($_POST["email"], $_POST["password"], $_POST["first_name"], $_POST["last_name"]);
                 $ret = $um->create($user);
-
+                //Si l'écriture en base de données réussi, on redirige vers la liste des utilisateurs
                 if ($ret) {
                     unset($_SESSION["errors"]);
                     header("Location: index.php?route=list-users");
@@ -133,15 +115,10 @@ class UserController extends AbstractController
             header("Location: index.php?route=create-user");
         }
     }
-
-
     public function delete(int $id) : void
     {
         $um = new UserManager();
         $um->delete($id);
         header("Location: index.php?route=list-users");
     }
-
-
-
 }
--- managers/UserManager.php
+++ managers/UserManager.php
@@ -6,26 +6,20 @@ class UserManager extends AbstractManager
     {
         parent::__construct();
     }
-
     public function findAll(): array
     {
         $query = $this->db->prepare('SELECT * FROM users');
         $parameters = [];
         $query->execute($parameters);
-
         $results = $query->fetchAll(PDO::FETCH_ASSOC);
-
         $users = [];
         foreach ($results as $result) {
             $user = new User($result['email'], $result['password'], $result['first_name'], $result['last_name'], $result['id']);
 
             $users[] = $user;
         }
-
         return $users;
     }
-
-
     public function findOne(int $id): ?User
     {
         $query = $this->db->prepare('SELECT * FROM users WHERE id = :id');
@@ -33,9 +27,7 @@ class UserManager extends AbstractManager
             ':id' => $id
         ];
         $query->execute($parameters);
-
         $result = $query->fetch(PDO::FETCH_ASSOC);
-
         if ($result) {
             $user = new User($result['email'], $result['password'], $result['first_name'], $result['last_name'], $result['id']);
             return $user;
@@ -43,52 +35,38 @@ class UserManager extends AbstractManager
             return null;
         }
     }
-
-
     public function findByEmail(string $email): ?User
     {
         $query = $this->db->prepare('SELECT * FROM users WHERE email = :email');
         $parameters = [
-            ':email' => $email
+            'email' => $email
         ];
         $query->execute($parameters);
-
         $result = $query->fetch(PDO::FETCH_ASSOC);
-
         if ($result) {
             $user = new User($result['email'], $result['password'], $result['first_name'], $result['last_name'], $result['id']);
             return $user;
         } else {
             return null;
         }
-
     }
-
-
     public function create(User $user) : bool {
-
         $query = $this->db->prepare("INSERT INTO users (id, email, password, first_name, last_name) VALUES (NULL, :email, :password, :first_name, :last_name)");
-
         $parameters = [
             'email' => $user->getEmail(),
             'password' => $user->getPassword(),
             'first_name' => $user->getFirstName(),
             'last_name' => $user->getLastName()
         ];
-
         $query->execute($parameters);
-
         if($this->db->lastInsertId()) {
             return true;
         } else {
             return false;
         }
     }
-
     public function update(User $user) : bool {
-
         $query = $this->db->prepare('UPDATE users SET email = :email, password = :password, first_name = :first_name, last_name = :last_name WHERE id = :id');
-
         $parameters = [
             'id' => $user->getId(),
             'email' => $user->getEmail(),
@@ -96,13 +74,9 @@ class UserManager extends AbstractManager
             'first_name' => $user->getFirstName(),
             'last_name' => $user->getLastName()
         ];
-
         $query->execute($parameters);
-
         return true;
-
     }
-
     public function delete(int $id) : bool {
         $query = $this->db->prepare("DELETE FROM users WHERE id = :id");
         $parameters = [
@@ -111,7 +85,5 @@ class UserManager extends AbstractManager
         $query->execute($parameters);
         return true;
     }
-
-
 }
 
